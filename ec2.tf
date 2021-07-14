95504c9c9ab603811f31203c2d16cd5e1d6cd6a8
mahendragayam <gayam.mahendra@gmail.com>
2021-07-14 22:30:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,11 @@ provider "aws" {
   secret_key = "ldPxcBu0pCimt0z9dFTDU3UB4iOCZDroHfiCsmZm"
 }
 
-resource "aws_instance" "myec2" {
-   ami = "ami-0dc2d3e4c0f9ebd18"
-   instance_type = "t2.micro"
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-1a"
+  size              = 5
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
