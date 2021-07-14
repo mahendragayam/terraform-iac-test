0010c2b6651e048258747c9322b46f02da056ce9
mahendragayam <gayam.mahendra@gmail.com>
2021-07-14 18:55:26 +0530

    new changes added

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
-  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+  region     = "us-east-1"
+  access_key = "AKIAZO6QNVD5BV755SEG"
+  secret_key = "BgN260i7nOJ2qr3jFuhcIXOnq3bOPayG4Pu+eW6L"
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
+   ami = "ami-0dc2d3e4c0f9ebd18"
    instance_type = "t2.micro"
 }
